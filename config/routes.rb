a603067b31cdb907eadc7d0c3bc60ba096f165d5
urilixo <falecomelvio@hotmail.com>
2022-03-10 19:41:26 -0300

    Change root to be events#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   resources :users
-  root to: "home#index"
+  root to: 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
