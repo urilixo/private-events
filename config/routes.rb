eea278da3ebc464827cc299140618d413b47f31b
urilixo <falecomelvio@hotmail.com>
2022-03-15 19:27:53 -0300

    Changes route of invite resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: :registrations }
   resources :events
   resources :users
-  resources :invites
+  resource :invites, only: %i[new create destroy]
   root to: 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
