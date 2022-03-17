ef0e76a3eb9bee7d5eaa8c431379e7d54553eea6
urilixo <falecomelvio@hotmail.com>
2022-03-16 21:28:01 -0300

    Add description column to event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_182122) do
+ActiveRecord::Schema.define(version: 2022_03_17_002443) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_182122) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.string "description"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
