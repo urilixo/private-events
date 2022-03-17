d5c3d32d8f006e0dbb245f300e39119ebae890b3
urilixo <falecomelvio@hotmail.com>
2022-03-16 23:53:12 -0300

    Add description column to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -67,6 +67,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :location, :date)
+      params.require(:event).permit(:title, :location,:description, :date)
     end
 end
